c3cd7566092daf3d76e0e468dc9bd752532da369
xbgmaster <xbgalejandro@gmail.com>
2025-07-28 03:29:27 -0500

    modified files all forms and javascript

--- javascript/budget.js
+++ javascript/budget.js
@@ -1,16 +1,71 @@
 function budget() {
     const incomeValue = document.getElementById('income').value;
-    var objExpenses = [{ income: incomeValue }];
+    const foodValue = document.getElementById('food-input').value;
+    const transportationValue = document.getElementById('transportation-input').value;
+    const enterteimentValue = document.getElementById('enterteiment-input').value;
+    const otherValue = document.getElementById('other-input').value;
+    var objExpenses = [{ income: incomeValue, food: foodValue, transportation: transportationValue, enterteiment: enterteimentValue, other: otherValue }];
 
-    if (incomeValue == '') {
+    if (incomeValue == '' || foodValue == '' || transportationValue == '' || enterteimentValue == '' || otherValue == '') {
         alert("Please fill out all fields.");
     } else {
         console.log("objeto ingresado:", objExpenses);
-         addBudget(objExpenses);
+        addBudget(objExpenses);
     }
 }
 
 function addBudget(objBudget) {
+    console.log("my obj budget in budget.html is " + JSON.stringify(objBudget));
     localStorage.setItem("budgetItems", JSON.stringify(objBudget));
     window.location.href = "index.html";
+}
+
+window.addEventListener("load", function () {
+    const budgetItems = JSON.parse(localStorage.getItem("budgetItems") || "[]");
+    console.log("budget memory item" + budgetItems);
+    let listBudget = document.querySelector("#income");
+    let listFood = document.querySelector("#food-input");
+    let listTranspotation = document.querySelector("#transportation-input");
+    let listEntertaiment = document.querySelector("#enterteiment-input");
+    let listOther = document.querySelector("#other-input");
+
+
+    budgetItems.forEach((item) => {
+        console.log(item.income)
+        listBudget.value = item.income;
+        listFood.value = item.food;
+        listTranspotation.value = item.transportation;
+        listEntertaiment.value = item.enterteiment;
+        listOther.value = item.other;
+    });
+
+    const budgetItemsNew = JSON.parse(localStorage.getItem("budgetStatus") || "[]");
+
+
+
+    let listFoodSpend = document.querySelector("#foodSpend");
+    let listTranspotationSpend = document.querySelector("#transportationSpend");
+    let listEntertaimentSpend = document.querySelector("#enterteimentSpend");
+    let listOtherSpend = document.querySelector("#otherSpend");
+
+    listFoodSpend.innerHTML = budgetItemsNew[0]?.foodCategory ?? 0;
+    listTranspotationSpend.innerHTML = budgetItemsNew[0]?.transportationCategory ?? 0;
+    listEntertaimentSpend.innerHTML = budgetItemsNew[0]?.enterteimentCategory ?? 0;
+    listOtherSpend.innerHTML = budgetItemsNew[0]?.otherCategory ?? 0;
+
+
+});
+
+function reset() {
+    let incomeValue = document.querySelector("#income");
+    let foodValue = document.querySelector("#food-input");
+    let transportationValue = document.querySelector("#transportation-input");
+    let enterteimentValue = document.querySelector("#enterteiment-input");
+    let otherValue = document.querySelector("#other-input");
+
+    incomeValue.value = '0';
+    foodValue.value = '0';
+    transportationValue.value = '0';
+    enterteimentValue.value = '0';
+    otherValue.value = '0';
 }
--- javascript/index.js
+++ javascript/index.js
@@ -6,20 +6,29 @@ window.addEventListener("load", function () {
 
     const budgetItems = JSON.parse(localStorage.getItem("budgetItems") || "[]");
     console.log(budgetItems);
-    let listBudget = document.querySelector("#current-balance");
-
-
-    budgetItems.forEach((item) => {
-    listBudget.textContent = item.income;        
-    });
-
-
-
-    //
-
 
     const $list = document.querySelector(".expense-list");
     $list.innerHTML = "";
+
+    const $listCategory = document.querySelector(".category-status-list");
+    $listCategory.innerHTML = "";
+
+    // rendering category
+    // Primero agrupamos montos por categoría
+    const categoryTotals = {};
+
+    savedItems.forEach((item) => {
+        const category = item.category;
+        const amount = parseFloat(item.amount) || 0;
+
+        categoryTotals[category] = (categoryTotals[category] || 0) + amount;
+    });
+
+    // Limpiamos el UL para evitar duplicados
+    $listCategory.innerHTML = "";
+
+
+
 
     console.log("list expenses" + $list);
 
@@ -33,23 +42,181 @@ window.addEventListener("load", function () {
                 <span class="date"><b>Purchase date:</b> ${item.date}</span> 
     `;
         $list.appendChild(li);
+
+        //quick budget status
+        const liBudget = document.createElement("li");
+        li.className = "budget-item";
+
+        console.log("item category " + item.category);
+
+
+        if (item.category == "Food & Dining") {
+            const foodBudget = budgetItems[0]?.food ?? 500;
+            const percentage = (item.amount / foodBudget) * 100;
+            const statusText = percentage < 100 ? "✅ On track" : "⚠️ Over budget!";
+
+            liBudget.innerHTML = `
+            <li class="category-status-item">
+                <span class="icon">🍕 <b>Food & Dining:</b> </span>
+                ${item.category}: 
+                <span class="amount" id="food-span" data-cat="food">
+                ${item.amount}/$${foodBudget}
+                </span>
+                <span class="state ${percentage < 100 ? 'ok' : 'alert'}">${statusText}</span>
+            </li>`;
+
+
+        }
+
+        if (item.category == "Transportation") {
+            const transportationBudget = budgetItems[0]?.transportation ?? 500;
+            const percentage = (item.amount / transportationBudget) * 100;
+            const statusText = percentage < 100 ? "✅ On track" : "⚠️ Over budget!";
+
+
+            liBudget.innerHTML = `
+            <li class="category-status-item">
+                <span class="icon">🚌 <b>Transportation:</b> </span>
+                ${item.category}: 
+                <span class="amount" id="transportation-span" data-cat="transportation">
+                ${item.amount}/$${transportationBudget}
+                </span>
+                <span class="state ${percentage < 100 ? 'ok' : 'alert'}">${statusText}</span>
+            </li>`;
+        }
+
+
+        if (item.category == "Entertainment") {
+            const enterteimentBudget = budgetItems[0]?.enterteiment ?? 500;
+            const percentage = (item.amount / enterteimentBudget) * 100;
+            const statusText = percentage < 100 ? "✅ On track" : "⚠️ Over budget!";
+
+
+            liBudget.innerHTML = `
+            <li class="category-status-item">
+                <span class="icon">🎮 <b>Enterteiment:</b> </span>
+                ${item.category}: 
+                <span class="amount" id="enterteiment-span" data-cat="enterteiment">
+                ${item.amount}/$${enterteimentBudget}
+                </span>
+                <span class="state ${percentage < 100 ? 'ok' : 'alert'}">${statusText}</span>
+            </li>`;
+        }
+
+        if (item.category == "Other") {
+
+            const otherBudget = budgetItems[0]?.enterteiment ?? 500;
+            const percentage = (item.amount / otherBudget) * 100;
+            const statusText = percentage < 100 ? "✅ On track" : "⚠️ Over budget!";
+
+
+            liBudget.innerHTML = `
+            <li class="category-status-item">
+                <span class="icon">🚌🎮 <b>Other:</b> </span>
+                ${item.category}: 
+                <span class="amount" id="other-span" data-cat="other">
+                ${item.amount}/$${otherBudget}
+                </span>
+                <span class="state ${percentage < 100 ? 'ok' : 'alert'}">${statusText}</span>
+            </li>`;
+        }
+
+        //1
+        //$listCategory.appendChild(liBudget);
     });
+
+    // group by items 
+    for (const category in categoryTotals) {
+        const liBudget = document.createElement("li");
+        liBudget.className = "category-status-item";
+
+        let icon = "📦";
+        let defaultBudget = 500;
+
+        switch (category) {
+            case "Food & Dining":
+                icon = "🍕";
+                defaultBudget = budgetItems[0]?.food ?? 500;
+                break;
+            case "Transportation":
+                icon = "🚌";
+                defaultBudget = budgetItems[0]?.transportation ?? 500;
+                break;
+            case "Entertainment":
+                icon = "🎮";
+                defaultBudget = budgetItems[0]?.entertainment ?? 500;
+                break;
+            case "Other":
+                icon = "📦";
+                defaultBudget = budgetItems[0]?.other ?? 500;
+                break;
+        }
+
+
+        let newObjetCategory = [{
+            foodCategory: categoryTotals["Food & Dining"],
+            transportationCategory: categoryTotals["Transportation"],
+            enterteimentCategory: categoryTotals["Entertainment"],
+            otherCategory: categoryTotals["Other"]
+        }];
+
+        localStorage.setItem("budgetStatus", JSON.stringify(newObjetCategory));
+
+
+        const total = categoryTotals[category];
+
+        console.log("el valor por budget status : " + total);
+
+        const percentage = (total / defaultBudget) * 100;
+        const statusText = percentage < 100 ? "✅ On track" : "⚠️ Over budget!";
+        const statusClass = percentage < 100 ? "ok" : "alert";
+
+        liBudget.innerHTML = `
+            <span class="icon">${icon} <b>${category}:</b></span>
+            <span class="amount" data-cat="${category.toLowerCase().replace(/\s+/g, '-')}">
+            $${total}/$${defaultBudget}
+            </span>
+            <span class="state ${statusClass}">${statusText}</span>
+            `;
+        //2
+        $listCategory.appendChild(liBudget);
+
+
+    }
 
 
 
     // index.html
     const savedTotal = localStorage.getItem("totalAmount") || "0.00";
+    console.log("object budget " + savedTotal);
+
+    let currentBalanceEl = document.querySelector("#current-balance");
+
+
+    let initialBalanceEl = document.querySelector("#initial-balance");
+    initialBalanceEl.textContent = 2400;
+
+
+
+
+    let currentBalanceInt = budgetItems[0]?.income ?? 2400;
+    let spentAmount = parseFloat(savedTotal) || 0;
+
+    console.log("current balance" + currentBalanceInt + "spentAmount " + spentAmount)
+
+    let updatedBalance = initialBalanceEl.textContent - spentAmount;
+    currentBalanceEl.textContent = updatedBalance;
+    console.log("my current balance is " + currentBalanceInt);
+
+
     let amount = document.querySelector(".monthly-spend");
-    let currentBalance = document.querySelector("#current-balance").textContent;
     let progress = document.querySelector("#progress-text");
 
+    let monthlyBudget = budgetItems[0]?.monthlyBudget ?? 500;
 
-    console.log(currentBalance);
+    progress.textContent = ((savedTotal / currentBalanceInt) * 100).toFixed(2) + "% used";
 
-    amount.textContent = "This Month: Spent: $" + savedTotal + " / Budget: " + currentBalance;
-    progress.textContent = ((savedTotal / currentBalance) * 100).toFixed(2) + "% used";
+    amount.textContent = "This Month: Spent: $" + savedTotal + " / Budget: " + currentBalanceInt;
 
-    console.log("saved " + (savedTotal) + "currentbalance " + parseInt(currentBalance));
-    console.log(savedTotal);
-
+    console.log("saved " + savedTotal + " current balance " + currentBalanceInt);
 });
